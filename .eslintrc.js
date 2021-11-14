49f26b8513add439091be7ae88608e0e0ff0b69e
ihaback <ihgit@fastmail.com>
2021-11-14 23:36:23 +0100

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,23 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {},
+  plugins: ['react', '@typescript-eslint', 'prettier',  'react-hooks'],
+  rules: {
+    'eqeqeq': 'error',
+    'no-console': 'warn',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/display-name': 'off',
+    'react/no-children-prop': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
   settings: {
     react: {
       pragma: 'React',
